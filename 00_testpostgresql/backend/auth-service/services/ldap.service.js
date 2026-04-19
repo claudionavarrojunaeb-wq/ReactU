dbd1c079e8e61b1a9e8bf0f56a472da6385ad7a2
Claudio Navarro <claudio.navarro@junaeb.cl>
2026-04-18 20:19:05 -0400

    202604182018 Pega

--- 00_testpostgresql/backend/auth-service/services/ldap.service.js
+++ 00_testpostgresql/backend/auth-service/services/ldap.service.js
@@ -1,41 +1,39 @@
-import ldap from "ldapts";
+import { Client } from "ldapts";
 
 const LDAP_URL = "ldap://10.16.200.221:389";
 
+export async function authenticate(username, password) {
+  const client = new Client({
+    url: LDAP_URL,
+  });
+
+  const upn = `${username}@junaeb.local`;
+
+  try {
+    await client.bind(upn, password);
+
+    return {
+      success: true,
+      message: "Login LDAP exitoso",
+    };
+  } catch (err) {
+    throw new Error("Credenciales inválidas");
+  } finally {
+    await client.unbind();
+  }
+}
+
 // export function authenticate(username, password) {
 //   return new Promise((resolve, reject) => {
-//     const client = ldap.createClient({
-//       url: LDAP_URL,
-//     });
 
-//     const upn = `${username}@junaeb.local`;
-
-//     client.bind(upn, password, (err) => {
-//       client.unbind();
-
-//       if (err) {
-//         return reject(err);
-//       }
-
-//       resolve({
+//     // 🔥 usuarios fake
+//     if (username === "admin" && password === "1234") {
+//       return resolve({
 //         success: true,
-//         message: "Login LDAP exitoso",
+//         message: "Login mock exitoso"
 //       });
-//     });
+//     }
+
+//     reject(new Error("Credenciales inválidas"));
 //   });
 // }
-
-export function authenticate(username, password) {
-  return new Promise((resolve, reject) => {
-
-    // 🔥 usuarios fake
-    if (username === "admin" && password === "1234") {
-      return resolve({
-        success: true,
-        message: "Login mock exitoso"
-      });
-    }
-
-    reject(new Error("Credenciales inválidas"));
-  });
-}
